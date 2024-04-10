c478baa5182d768664ab20eb97f82e19ccc78259
TheunisHurter <thurter@usaid.gov>
2024-04-10 14:49:14 +0200

    pushing up starter files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a test people
+
+---
+
+*Disclaimer: The findings, interpretation, and conclusions expressed herein are those of the authors and do not necessarily reflect the views of United States Agency for International Development. All errors remain our own.*
---
+++ Scripts/TESTTWO script.R
@@ -0,0 +1,28 @@
+#starter script
+# AUTHOR:   T Hurter | USAID South Africa
+# PURPOSE:  
+# REF ID:   280382b1 
+# LICENSE:  MIT
+# DATE:     2024-04-10
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  sipack
+
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  # SI specific paths/functions  
+    load_secrets()
+
+  # Grab metadata
+    get_metadata(filepath) 
+  
+  ref_id <- "280382b1"
+
+# IMPORT ------------------------------------------------------------------
+  
+  return_data
+  msd
+
+# MUNGE -------------------------------------------------------------------
